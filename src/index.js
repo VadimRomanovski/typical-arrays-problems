515b5c14646ec36352551795c2b46261217ba13d
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-03-17 15:43:24 +0300

    init: npm start typical-arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length == 0){
+      return 0;
+    }else{
+        function compareNumeric(a, b) {
+            if (a > b) return 1;
+            if (a == b) return 0;
+            if (a < b) return -1;
+            };
+        array.sort(compareNumeric);
+        return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length == 0){
+      return 0;
+    }else{
+        function compareNumeric(a, b) {
+            if (a > b) return -1;
+            if (a == b) return 0;
+            if (a < b) return 1;
+            };
+        array.sort(compareNumeric);
+        return array[0];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length == 0){
+      return 0;
+    }else{
+      let sum = array.reduce((value, currentItem) => value + currentItem)
+      return sum/array.length
+    }
 }
